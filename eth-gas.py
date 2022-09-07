b53e44be798f32ffde20936ab03d9266f9dada30
Nicholas Charriere <nicholascharriere@gmail.com>
2022-09-07 12:19:37 -0700

    Simple eth-gas command in python

---
+++ eth-gas.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ETH Gas
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon ⛽️
+# @raycast.packageName Ethereum
+#
+# Documentation:
+# @raycast.description Check the live Ethereum gas price from Etherscan
+# @raycast.author Nicholas Charriere
+# @raycast.authorURL https://twitter.com/nichochar
+
+import sys
+import requests 
+
+
+if __name__ == '__main__':
+    response = requests.get('https://gas.best/stats')
+    data = response.json()
+    live = f"{data['pending']['fee']} gwei"
+    one_hour = f"{data['forecast']['1 hour']} gwei"
+
+    result = f"{live} ⏰ ${one_hour} in the hour"
+    print(result)
+    sys.exit(0)
